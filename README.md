b1bd5147dda7891a2738a179a1ece134d720e3d8
Serhat ZENGİN <srhtzngn@hotmail.com>
2021-10-28 22:04:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# What_is_my_weight
+ 
---
+++ app/src/main/java/com/example/what_is_my_weight/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.what_is_my_weight
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
